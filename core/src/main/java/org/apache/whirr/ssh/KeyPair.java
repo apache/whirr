e8ea249223c6a2dcba16d70bfdb4eb73a173cda4
Thomas White <tomwhite@apache.org>
2010-12-15 04:52:51 +0000

    WHIRR-160. Improve SSH key diagnostics. Contributed by Andrei Savu.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/whirr/trunk@1049424 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/whirr/ssh/KeyPair.java
+++ core/src/main/java/org/apache/whirr/ssh/KeyPair.java
@@ -19,9 +19,12 @@
 package org.apache.whirr.ssh;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 
@@ -30,20 +33,53 @@ import com.jcraft.jsch.JSchException;
  */
 public class KeyPair {
 
+  public static Map<String, String> generate() throws JSchException {
+      return generate(null);
+  }
+
   /**
    * return a "public" -> rsa public key, "private" -> its corresponding
    *   private key
    */
-  public static Map<String,String> generate() throws JSchException {
+  public static Map<String,String> generate(String passPhrase)
+  throws JSchException {
     com.jcraft.jsch.KeyPair pair = com.jcraft.jsch.KeyPair.genKeyPair(
         new JSch(),  com.jcraft.jsch.KeyPair.RSA);
+    if (passPhrase != null) {
+      pair.setPassphrase(passPhrase);
+    }
+
     ByteArrayOutputStream publicKeyOut = new ByteArrayOutputStream();
     ByteArrayOutputStream privateKeyOut = new ByteArrayOutputStream();
+
     pair.writePublicKey(publicKeyOut, "whirr");
     pair.writePrivateKey(privateKeyOut);
+
     String publicKey = new String(publicKeyOut.toByteArray());
     String privateKey = new String(privateKeyOut.toByteArray());
+
     return ImmutableMap.<String, String> of("public", publicKey,
         "private", privateKey);
+  }
+
+  public static Map<String, File> generateTemporaryFiles()
+  throws JSchException, IOException {
+    return generateTemporaryFiles(null); 
+  }
+
+  public static Map<String, File> generateTemporaryFiles(String passPhrase)
+  throws JSchException, IOException {
+    Map<String, String> keys = KeyPair.generate(passPhrase);
+
+    File privateKeyFile = File.createTempFile("private", "key");
+    privateKeyFile.deleteOnExit();
+    Files.write(keys.get("private").getBytes(), privateKeyFile);
+
+    File publicKeyFile = new File(privateKeyFile.getAbsolutePath() + ".pub");
+    publicKeyFile.deleteOnExit();
+    Files.write(keys.get("public").getBytes(), publicKeyFile);
+
+    return ImmutableMap.<String, File> of("public", publicKeyFile,
+              "private", privateKeyFile);
   }
 }
