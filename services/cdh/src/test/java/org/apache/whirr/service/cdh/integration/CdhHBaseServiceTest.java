def4da74bcefaeafd957be646e86bba973f3a033
Frank Scholten <frankscholten@apache.org>
2012-05-10 19:29:24 +0000

    WHIRR-582. Yarn service does not build in IntelliJ because of missing Hadoop test dependencies (Frank Scholten)
    git-svn-id: https://svn.apache.org/repos/asf/whirr/trunk@1336843 13f79535-47bb-0310-9956-ffa450edef68

--- services/cdh/src/test/java/org/apache/whirr/service/cdh/integration/CdhHBaseServiceTest.java
+++ services/cdh/src/test/java/org/apache/whirr/service/cdh/integration/CdhHBaseServiceTest.java
@@ -30,6 +30,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,13 +40,13 @@ import static org.junit.Assert.assertTrue;
 
 public class CdhHBaseServiceTest {
 
-  private static final byte[] FIRST = Bytes.toBytes("");
-  private static final byte[] TABLE = Bytes.toBytes("testtable");
-  private static final byte[] ROW = Bytes.toBytes("testRow");
-  private static final byte[] FAMILY1 = Bytes.toBytes("testFamily1");
-  private static final byte[] FAMILY2 = Bytes.toBytes("testFamily2");
-  private static final byte[] COLUMN = Bytes.toBytes("testFamily1:testColumn");
-  private static final byte[] VALUE = Bytes.toBytes("testValue");
+  private static final ByteBuffer FIRST = ByteBuffer.wrap(Bytes.toBytes(""));
+  private static final ByteBuffer TABLE = ByteBuffer.wrap(Bytes.toBytes("testtable"));
+  private static final ByteBuffer ROW = ByteBuffer.wrap(Bytes.toBytes("testRow"));
+  private static final ByteBuffer FAMILY1 = ByteBuffer.wrap(Bytes.toBytes("testFamily1"));
+  private static final ByteBuffer FAMILY2 = ByteBuffer.wrap(Bytes.toBytes("testFamily2"));
+  private static final ByteBuffer COLUMN = ByteBuffer.wrap(Bytes.toBytes("testFamily1:testColumn"));
+  private static final ByteBuffer VALUE = ByteBuffer.wrap(Bytes.toBytes("testValue"));
 
   private static HBaseServiceController controller =
     HBaseServiceController.getInstance("whirr-hbase-cdh-test.properties");
